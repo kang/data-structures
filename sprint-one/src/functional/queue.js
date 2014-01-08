eee401a91957d59278acb6b31dea96b010893223
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-08 15:37:27 -0800

    finished functional queue, using length of :object.keys

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,22 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
-
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size]=value;
+    size++;
   };
 
   instance.dequeue = function(){
+    var queue = Object.keys(storage);
+    var result = storage[queue[0]];
+    delete storage[queue[0]];
+    return result;
   };
 
   instance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return instance;
