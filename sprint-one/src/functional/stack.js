0887f21158b4f7c88534bef006c95a9153bbd644
brianlk <brian.kang2@gmail.com>
2014-01-08 11:39:02 -0800

    added incrementation/decrementation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,20 +1,24 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
+  var storage = {};
   /* START PROMPT
     var size; // Hint: set an initial value here
   -END PROMPT */
+  var size = 0;
 
   // Implement the methods below
   instance.push = function(value){
+    size++;
   };
 
   instance.pop = function(){
+    size--;
   };
 
   instance.size = function(){
+    return size;
   };
   
     return instance;
