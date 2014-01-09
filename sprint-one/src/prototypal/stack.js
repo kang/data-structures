2e31d73a53c9e6718f3a1e940be5721b0f2fe7a1
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-08 20:36:50 -0800

    finished prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,28 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance._storage = {};
+  instance._size = 0;
+  return instance;
 };
 
 var stackMethods = {};
+
+// Implement the methods below
+stackMethods.push = function(value){
+  this._size++;
+  this._storage[this._size-1] = value;
+};
+stackMethods.pop = function(){
+  var popped = this._storage[this._size-1];
+  delete this._storage[this._size-1];
+  if(this._size) {
+    this._size--;
+  }
+  return popped;
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
