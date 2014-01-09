47b2575a127355ff7beca111f43acb21804874c2
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-09 11:18:48 -0800

    mostly finished linkedlists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,37 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.tail){
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail=makeNode(value);
+    }
+    if(list.head===null){
+      list.head=list.tail;
+    }
   };
 
   list.removeHead = function(){
+    var tmp;
+    if(list.head.next){
+      tmp = list.head.next;
+    }
+    delete list.head;
+    list.head = tmp;
   };
 
   list.contains = function(target, node){
+    var doesContain = false;
+    for(var objs in list){
+      for(var nodes in objs){
+        if(nodes===target){
+          doesContain=true;
+        }
+      }
+    }
+    return doesContain;
   };
-
   return list;
 };
 
