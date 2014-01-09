919f0dcdab5bd912c1bae7b97db4f249859a5325
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-08 21:02:51 -0800

    completed prototypal

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,25 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  this._storage={};
+  this._size=0;
+  this._removed=0;
 };
+
+Queue.prototype.enqueue = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Queue.prototype.dequeue = function(){
+  var result = this._storage[this._removed];
+  delete this._storage[this._removed];
+  if(this._size-this._removed){
+    this._removed++;
+  }
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this._size-this._removed;
+};
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,25 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  this._storage = {};
+  this._size = 0;
 };
+
+// Implement the methods below
+Stack.prototype.push = function(value){
+  this._size++;
+  this._storage[this._size-1] = value;
+};
+Stack.prototype.pop = function(){
+  var popped = this._storage[this._size-1];
+  delete this._storage[this._size-1];
+  if(this._size) {
+    this._size--;
+  }
+  return popped;
+};
+
+Stack.prototype.size = function(){
+  return this._size;
+};
+
+var newStack = new Stack();
