49ad83c4e9263040217e6d3794146d528c87bd70
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-08 20:40:06 -0800

    completed prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,29 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+  instance._storage = {};
+  instance._size = 0;
+  instance._removed = 0;
+  return instance;
 };
 
 var queueMethods = {};
+
+
+queueMethods.enqueue = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+queueMethods.dequeue = function(){
+  // var queue = Object.keys(this._storage);
+  var result = this._storage[this._removed];
+  delete this._storage[this._removed];
+  if(this._size-this._removed){
+    this._removed++;
+  }
+  return result;
+};
+
+queueMethods.size = function(){
+  return this._size-this._removed;
+};
