42c7f3c4d6cdf53807cfcaaaa73610238484e5b4
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-10 15:39:10 -0800

    fixed collision problem in hashtable(is actually a hashtable now)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,31 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._stored = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if(Object.keys(this._storage).length>this._limit-1){
+  if(this._stored*4>=this._limit*3){
     this._limit=this._limit*2;
   }
-  this._storage[i]=v;
+  if(Array.isArray(this._storage[i])===false){
+    this._storage[i]=[];
+  }
+  this._storage[i].push([k,v]);
+  this._stored++;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   if(this._storage[i]){
-    return this._storage[i];
+    for(var j = 0; j < this._storage[i].length; j++){
+      if(this._storage[i][j]===undefined){
+        return null;
+      } else if (this._storage[i][j][0]===k){
+        return this._storage[i][j][1];
+      }
+    }
   } else {
     return null;
   }
@@ -22,9 +33,15 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if(Object.keys(this._storage).length <= (this._limit/2)){
-    this._limit = this._limit/2;
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0]===k){
+      delete this._storage[i][j];
+    }
   }
-  delete this._storage[i];
+  // delete this._storage[i];
+  if(this._stored >= this._limit){
+    this._limit = this._stored/2;
+  }
+  this._stored--;
 };
 
