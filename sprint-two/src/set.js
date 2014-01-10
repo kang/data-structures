1b0b02d65c542fbd0cb426218e3e9d269252a86d
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-09 21:45:57 -0800

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,18 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage = this._storage || {};
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage.hasOwnProperty(item)){
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
