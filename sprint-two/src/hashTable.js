39e3e8bdfef35d95bfa03d9422f641ff0578c7e9
Hack Reactor Students <brian.kang2@gmail.com>
2014-01-09 21:59:03 -0800

    completed hashtable basic specs(need to explore helpers)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,17 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[k]=v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[k];
 };
 
 HashTable.prototype.remove = function(k){
+  console.log(this._storage);
+  this._storage[k] = null;
+  console.log(this._storage);
 };
 
